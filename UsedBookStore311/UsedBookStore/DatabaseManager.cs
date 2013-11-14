a52a7aaf7f6a05cdb5811dfc9347cc4dae2fc26a
Eric <kowaitori@gmail.com>
2013-11-13 19:41:30 -0500

    Changed the Image format that was being converted to raw byte data to reflect the chosen image format instead of GIF.

--- UsedBookStore311/UsedBookStore/DatabaseManager.cs
+++ UsedBookStore311/UsedBookStore/DatabaseManager.cs
@@ -60,7 +60,7 @@ namespace UsedBookStore
 		public static byte[] imageToByteArray(System.Drawing.Image imageIn)
 		{
 			MemoryStream ms = new MemoryStream();
-			imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+			imageIn.Save(ms, imageIn.RawFormat);
 			return ms.ToArray();
 		}
 
